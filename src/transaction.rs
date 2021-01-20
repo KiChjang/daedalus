fe82ec485d37ce439e6eec60bc872cb9ddaf146d
Keith Yeung <kungfukeith11@gmail.com>
2021-01-20 06:31:56 -0800

    Add deserialization test for transactions

--- src/transaction.rs
+++ src/transaction.rs
@@ -30,3 +30,104 @@ pub enum TransactionType {
     #[serde(alias = "chargeback")]
     Chargeback,
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use csv::ReaderBuilder;
+
+    #[test]
+    fn test_parse_deposit() {
+        let data = "\
+type,client,tx,amount\n
+deposit,1,1,2.0";
+
+        let mut rdr = ReaderBuilder::new()
+            .flexible(true)
+            .from_reader(data.as_bytes());
+
+        for res in rdr.deserialize::<Transaction>() {
+            let tx = res.unwrap();
+            assert_eq!(tx.ty, TransactionType::Deposit);
+            assert_eq!(tx.client_id, 1);
+            assert_eq!(tx.id, 1);
+            assert_eq!(tx.amount, Some(2.0));
+        }
+    }
+
+    #[test]
+    fn test_parse_withdrawal() {
+        let data = "\
+type,client,tx,amount\n
+withdrawal,1,1,2.0";
+
+        let mut rdr = ReaderBuilder::new()
+            .flexible(true)
+            .from_reader(data.as_bytes());
+
+        for res in rdr.deserialize::<Transaction>() {
+            let tx = res.unwrap();
+            assert_eq!(tx.ty, TransactionType::Withdrawal);
+            assert_eq!(tx.client_id, 1);
+            assert_eq!(tx.id, 1);
+            assert_eq!(tx.amount, Some(2.0));
+        }
+    }
+
+    #[test]
+    fn test_parse_dispute() {
+        let data = "\
+type,client,tx,amount\n
+dispute,1,1";
+
+        let mut rdr = ReaderBuilder::new()
+            .flexible(true)
+            .from_reader(data.as_bytes());
+
+        for res in rdr.deserialize::<Transaction>() {
+            let tx = res.unwrap();
+            assert_eq!(tx.ty, TransactionType::Dispute);
+            assert_eq!(tx.client_id, 1);
+            assert_eq!(tx.id, 1);
+            assert_eq!(tx.amount, None);
+        }
+    }
+
+    #[test]
+    fn test_parse_resolve() {
+        let data = "\
+type,client,tx,amount\n
+resolve,1,1";
+
+        let mut rdr = ReaderBuilder::new()
+            .flexible(true)
+            .from_reader(data.as_bytes());
+
+        for res in rdr.deserialize::<Transaction>() {
+            let tx = res.unwrap();
+            assert_eq!(tx.ty, TransactionType::Resolve);
+            assert_eq!(tx.client_id, 1);
+            assert_eq!(tx.id, 1);
+            assert_eq!(tx.amount, None);
+        }
+    }
+
+    #[test]
+    fn test_parse_chargeback() {
+        let data = "\
+type,client,tx,amount\n
+chargeback,1,1";
+
+        let mut rdr = ReaderBuilder::new()
+            .flexible(true)
+            .from_reader(data.as_bytes());
+
+        for res in rdr.deserialize::<Transaction>() {
+            let tx = res.unwrap();
+            assert_eq!(tx.ty, TransactionType::Chargeback);
+            assert_eq!(tx.client_id, 1);
+            assert_eq!(tx.id, 1);
+            assert_eq!(tx.amount, None);
+        }
+    }
+}
